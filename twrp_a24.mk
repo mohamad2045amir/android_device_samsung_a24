0729389ed03578ddf898da3641db49c2238d55a0
mohamad2045amir <mohamad2045amir@gmail.com>
2025-02-19 22:04:43 +0330

    Update and rename omni_a24.mk to twrp_a24.mk

--- twrp_a24.mk
+++ twrp_a24.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a24 device
 $(call inherit-product, device/samsung/a24/device.mk)
 
 PRODUCT_DEVICE := a24
-PRODUCT_NAME := omni_a24
+PRODUCT_NAME := twrp_a24
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A245F
 PRODUCT_MANUFACTURER := samsung
